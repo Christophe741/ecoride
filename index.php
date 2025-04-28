424e294430c14ea8e46b4a37dff36eaa0b416d2f
Christophe <chris.cw@outlook.fr>
2025-04-28 18:05:20 +0200

    Styliser la page d’accueil avec CSS

--- index.php
+++ index.php
@@ -5,23 +5,26 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>EcoRide - Accueil</title>
   <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+
 </head>
 <body>
 
   <header>
-    <div class="logo">EcoRide</div>
+    <a href="index.php" class="logo"><span class="green">E</span>co<span><span class="green">R</span>ide</span></a>
     <nav>
       <ul>
         <li><a href="recherche.php">Covoiturage</a></li>
         <li><a href="inscription.php">Créer un compte</a></li>
         <li><a href="connexion.php">Se connecter</a></li>
+        <li><a href="contact.php">Contact</a></li>
       </ul>
     </nav>
   </header>
-
-  <section class="search-section">
+<main>
+  <section class="hero-search">
     <h1>Trouvez votre prochain covoiturage</h1>
-    <form action="recherche.php" method="get">
+    <form class="search-form" action="recherche.php" method="get">
       <input type="text" name="depart" placeholder="Ville de départ">
       <input type="text" name="arrivee" placeholder="Ville d'arrivée">
       <input type="date" name="date">
@@ -29,19 +32,20 @@
     </form>
   </section>
 
-  <section class="presentation">
+  <section class="hero-presentation">
     <h2>Réinventons les trajets du quotidien</h2>
     <p>
-      EcoRide est une plateforme de covoiturage pensée pour ceux qui veulent bouger autrement.
+      EcoRide est une plateforme de covoiturage pensée pour ceux qui veulent bouger autrement.<br>
       Ensemble, réduisons notre impact environnemental tout en rendant les trajets plus économiques et conviviaux.
     </p>
-    <img src="assets/images/voiture.png" alt="Illustration d'une voiture écologique">
+    <img src="assets/images/voiture.png" class="car-image" alt="Illustration d'une voiture écologique">
   </section>
-
+</main>
   <footer>
-    <p>Mail : <a href="mailto:EcoRide@fakemail.com">EcoRide@fakemail.com</a></p>
-    <a href="mentions-legales.html">Mentions légales</a>
+  <div class="logo"><span class="green">E</span>co<span><span class="green">R</span>ide</span></div>
+  <a href="mailto:EcoRide@fakemail.com" class="mail">EcoRide@fakemail.com</a>
+    <a href="mentions-legales.html" class="mentions">Mentions légales</a>
   </footer>
-test4
+
 </body>
 </html>
