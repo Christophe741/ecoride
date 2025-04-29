0e5218679ab0cdcc4c17ae0d685554c93668aa68
Christophe <chris.cw@outlook.fr>
2025-04-29 16:11:32 +0200

    Ajout de la page covoiturage

---
+++ covoiturage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>EcoRide - Accueil</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+
+</head>
+<body>
+<?php require_once 'includes/header.php';?>
+<?php require_once 'includes/hero_search.php';?>
+
+<main>
+
+
+  
+</main>
+
+<?php require_once 'includes/footer.php'; ?>
+  
+</body>
+</html>
