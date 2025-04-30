cf6dd2dc62d7006c1600fc20de9630c6c6db0f41
Christophe <chris.cw@outlook.fr>
2025-04-30 19:48:43 +0200

    changement de nom

--- includes/hero_search.php
+++ includes/hero_search.php
@@ -1,6 +1,6 @@
 <section class="hero-search">
     <h1>Trouvez votre prochain covoiturage</h1>
-    <form class="search-form" action="recherche.php" method="get">
+    <form class="search-form" action="covoiturage.php" method="get">
       <input type="text" name="depart" placeholder="Ville de départ">
       <input type="text" name="arrivee" placeholder="Ville d'arrivée">
       <input type="date" name="date">
