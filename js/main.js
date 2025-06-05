683e8bd8c7b9d6e5f866ec34740aa3c4e853baeb
Christophe <chris.cw@outlook.fr>
2025-06-05 14:43:18 +0200

    modif

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 import { domReady } from "./domReady.js";
 import { fixFouc } from "./fouc-fix.js";
+import { toggler } from "./toggler.js";
 
 domReady(() => {
   fixFouc();
+  const icon = document.querySelector("#toggler");
+  if (icon) {
+    icon.addEventListener("click", toggler);
+  }
 });
