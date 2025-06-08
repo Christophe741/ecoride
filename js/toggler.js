242c21aa93a88f00d01d81a55835c9f0bcd9c823
Christophe <chris.cw@outlook.fr>
2025-06-08 17:38:52 +0200

    Enlever le scroll sur le menu burger

--- js/toggler.js
+++ js/toggler.js
@@ -1,11 +1,15 @@
 export function toggler() {
   const icon = document.querySelector("#toggler");
   const menu = document.querySelector(".menu");
+  const body = document.body;
+
   if (icon.innerHTML == "menu") {
     icon.innerHTML = "close";
     menu.style.display = "flex";
+    body.style.overflow = "hidden";
   } else {
     icon.innerHTML = "menu";
     menu.style.display = "none";
+    body.style.overflow = "";
   }
 }
