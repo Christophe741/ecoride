e2971e8b113a7b485e5ca85776ca24fd513aa804
Christophe <chris.cw@outlook.fr>
2025-06-17 18:03:23 +0200

    Mise en place de docker

--- db/config.php
+++ db/config.php
@@ -12,10 +12,10 @@ if (file_exists($envFile)) {
             }
         }
 
-$host = 'localhost';  
-$dbname = 'ecoride';
-$user = getenv('DB_USER') ?: 'visiteur';
-$pass = getenv('DB_PASS') ?: '';
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
