f5933bc9817707d3370caa0b6020413c83390ff7
Christophe <chris.cw@outlook.fr>
2025-06-26 21:54:19 +0200

    Ajout mentions légales

---
+++ mentions-legales.php
@@ -0,0 +1,29 @@
+<?php
+$pageTitle = "Mentions légales";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<?php require_once 'includes/head.php';?>
+<body>
+<?php require_once 'includes/header.php'; ?>
+<main class="mentions-legales-container">
+    <h2>Mentions légales</h2>
+    <section id=editeur>
+        <h3>Éditeur du site</h3>
+        <p>Nom du site : EcoRide<br>
+        Responsable de la publication : Ecoride<br>
+        Adresse : 12 rue du Laurier<br>
+        Email : EcoRide@fakemail.com</p>
+    </section>
+    <section>
+        <h3>Propriété intellectuelle</h3>
+        <p>Tous les contenus présents sur ce site sont protégés par le droit d'auteur. Toute reproduction, même partielle, est interdite sans autorisation préalable.</p>
+    </section>
+    <section>
+        <h3>Données personnelles</h3>
+        <p>Les informations recueillies via les formulaires sont destinées uniquement à EcoRide et ne seront jamais transmises à des tiers sans consentement.</p>
+    </section>
+</main>
+<?php require_once 'includes/footer.php'; ?>
+</body>
+</html>
