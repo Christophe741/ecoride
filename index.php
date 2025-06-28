dc0d55f3c4394dd8eab2749f4a6536a2c4adce8e
Christophe <chris.cw@outlook.fr>
2025-06-28 19:19:04 +0200

    mise en place BEM

--- index.php
+++ index.php
@@ -4,24 +4,31 @@ $pageTitle = "EcoRide - Accueil";
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-<?php require_once 'includes/head.php';?>
+<?php require_once 'includes/head.php'; ?>
 
-<body class="page-index">
-  
-<?php require_once 'includes/header.php';?>
-<?php require_once 'includes/search_section.php';?>
+<body class="home-page">
 
-<main>
-  <section class="hero-presentation">
-    <h2>Réinventons les trajets du quotidien</h2>
-    <p>
-      EcoRide est une plateforme de covoiturage pensée pour ceux qui veulent bouger autrement.<br>
-      Ensemble, réduisons notre impact environnemental tout en rendant les trajets plus économiques et conviviaux.
-    </p>
-    <img src="assets/images/car.png" class="car-image" alt="Illustration d'une voiture écologique">
-  </section>
-</main>
+  <?php require_once 'includes/header.php'; ?>
 
-<?php require_once 'includes/footer.php'; ?>
+  <main class="home-page__main">
+
+    <?php require_once 'includes/search_section.php'; ?>
+
+    <section class="hero">
+      <div class="hero__content">
+        <h2 class="hero__title">Réinventons les trajets du quotidien</h2>
+        <p>
+          EcoRide est une plateforme de covoiturage pensée pour ceux qui veulent bouger autrement.<br>
+          Ensemble, réduisons notre impact environnemental tout en rendant les trajets plus économiques et conviviaux.
+        </p>
+      </div>
+      <div class="hero__illustration">
+        <img src="assets/images/car.png" class="car-image" alt="Illustration d'une voiture écologique">
+      </div>
+    </section>
+
+  </main>
+
+  <?php require_once 'includes/footer.php'; ?>
 </body>
 </html>
