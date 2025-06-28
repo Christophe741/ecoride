7cd2f5389042c7879286d7be9b247fc06502125e
Christophe <chris.cw@outlook.fr>
2025-06-28 22:32:41 +0200

    footer BEM & modif

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,9 @@
-<footer>
- <a href="index.php" class="logo hidemobile"><span class="green">E</span>co<span class="green">R</span>ide</a>
-  <a href="mailto:EcoRide@fakemail.com" class="mail">EcoRide@fakemail.com</a>
-    <a href="legal.php" class="mentions">Mentions légales</a>
-  </footer>
+<footer class="footer">
+  <a href="index.php" class="footer__logo footer__logo--desktop">
+    <span class="footer__logo-part footer__logo-part--green">E</span>co<span class="footer__logo-part footer__logo-part--green">R</span>ide
+  </a>
+  
+  <a href="mailto:EcoRide@fakemail.com" class="footer__mail">EcoRide@fakemail.com</a>
+  
+  <a href="legal.php" class="footer__legal">Mentions légales</a>
+</footer>
