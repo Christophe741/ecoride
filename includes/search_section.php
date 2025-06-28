6752ab680accb9b807e3c55e3fcb671a9f0e58d6
Christophe <chris.cw@outlook.fr>
2025-06-29 00:31:04 +0200

    search_section bem + modif

--- includes/search_section.php
+++ includes/search_section.php
@@ -1,9 +1,10 @@
-<section class="hero-search">
-    <h1>Trouvez votre prochain covoiturage</h1>
-    <form class="search-form" action="rides.php" method="get">
-      <input type="text" name="depart" placeholder="Ville de départ">
-      <input type="text" name="arrivee" placeholder="Ville d'arrivée">
-      <input type="date" name="date">
-      <button class="mobilebutton" type="submit">Rechercher</button>
-    </form>
+<section class="search-hero">
+  <h1 class="search-hero__title">Trouvez votre prochain covoiturage</h1>
+
+  <form class="form-search" action="rides.php" method="get">
+    <input class="form-search__input" type="text" name="depart" placeholder="Ville de départ">
+    <input class="form-search__input" type="text" name="arrivee" placeholder="Ville d'arrivée">
+    <input class="form-search__input" type="date" name="date">
+    <button class="form-search__button" type="submit">Rechercher</button>
+  </form>
 </section>
