d6df823f700fcaffcc6fd64733b55cb28a68455a
Christophe <chris.cw@outlook.fr>
2025-07-14 01:26:25 +0200

    modif

--- db/mongo-init.js
+++ db/mongo-init.js
@@ -1,11 +1,11 @@
 db = db.getSiblingDB("ecoride");
-db.users.insertMany([
+db.rides.insertMany([
   {
-    trajet_id: 5,
+    ride_id: 5,
     description: "Conducteur sérieux, bonne ambiance, ponctualité assurée.",
   },
   {
-    trajet_id: 4,
+    ride_id: 4,
     description:
       "Conductrice attentive et souriante, j'aime proposer des trajets confortables et agréables. Ambiance détendue, musique douce en fond, papotages si on en a envie, et bien sûr, petite pause café si besoin. Au plaisir de partager un trajet avec vous !",
   },
