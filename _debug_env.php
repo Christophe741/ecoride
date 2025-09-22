e59be5b018825b340c99cf7571a8b91bb3a83374
Christophe <chris.cw@outlook.fr>
2025-09-22 22:26:52 +0200

    modif

--- _debug_env.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: text/plain; charset=utf-8');
-$keys = ['DB_HOST','DB_NAME','DB_USER','DB_PASS'];
-foreach ($keys as $k) {
-  printf("%s=%s\n", $k, var_export(getenv($k), true));
-}
