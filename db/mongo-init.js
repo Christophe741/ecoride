2321421c05f969a88375350870518a7dd0aa90c6
Christophe <chris.cw@outlook.fr>
2025-09-23 21:40:53 +0200

    Suppression des identifiants en dur dans mongo-init.js

--- db/mongo-init.js
+++ db/mongo-init.js
@@ -1,9 +1,13 @@
-db = db.getSiblingDB("ecoride");
+const dbName = process.env.MONGO_DB;
+const appUser = process.env.MONGO_USER;
+const appPass = process.env.MONGO_PASS;
+
+db = db.getSiblingDB(dbName);
 
 db.createUser({
-  user: "app",
-  pwd: "secret",
-  roles: [{ role: "readWrite", db: "ecoride" }],
+  user: appUser,
+  pwd: appPass,
+  roles: [{ role: "readWrite", db: dbName }],
 });
 
 db.rides.insertMany([
@@ -14,6 +18,8 @@ db.rides.insertMany([
   {
     ride_id: 4,
     description:
-      "Conductrice attentive et souriante, j'aime proposer des trajets confortables et agréables. Ambiance détendue, musique douce en fond, papotages si on en a envie, et bien sûr, petite pause café si besoin. Au plaisir de partager un trajet avec vous !",
+      "Conductrice attentive et souriante, j'aime proposer des trajets confortables et agréables. " +
+      "Ambiance détendue, musique douce en fond, papotages si on en a envie, et bien sûr, petite pause café si besoin. " +
+      "Au plaisir de partager un trajet avec vous !",
   },
 ]);
