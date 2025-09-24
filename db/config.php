8d7e83c0bb737864822caefcbd46821bc532f6d9
Christophe <chris.cw@outlook.fr>
2025-09-24 20:11:58 +0200

    modif

--- db/config.php
+++ db/config.php
@@ -29,7 +29,7 @@ $mongoHost = $_ENV['MONGO_HOST'];
 $mongoDb   = $_ENV['MONGO_DB'];
 
 
-$mongoUri = "mongodb://{$mongoUser}:{$mongoPass}@{$mongoHost}:27017/{$mongoDb}?authSource=admin";
+$mongoUri = "mongodb://{$mongoUser}:{$mongoPass}@{$mongoHost}:27017/{$mongoDb}";
 
 try {
     $mongo = new MongoDB\Client($mongoUri);
