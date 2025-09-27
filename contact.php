dfa1f6754637c4c34e5e7571b48086833ac0822c
Christophe <chris.cw@outlook.fr>
2025-09-27 13:06:14 +0200

    stylisation formulaire

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@ $basePath = '/';
 <main class="form-container">
   <h1>Contactez-nous</h1>
   <form method="post" action="#">
-    <input type="text" name="nom" placeholder="Votre nom" required><br>
+    <input type="text" name="pseudo" placeholder="Votre nom" required><br>
     <input type="email" name="email" placeholder="Votre email" required><br>
     <textarea name="message" placeholder="Votre message" required></textarea><br>
     <button type="submit">Envoyer</button>
